515dde7e26096250415d6c246f643364f6830c18
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2019-11-16 20:37:31 -0500

    changed back port to just 8080

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var randomstring = require("randomstring");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
